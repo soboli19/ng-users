93120e68e2612541ebbfe41ba82407af1379bf15
ILIYA SOBOLEV <soboli@att.net>
2019-11-14 17:46:39 -0600

    Added delete

--- src/app/user-view/user-view.component.ts
+++ src/app/user-view/user-view.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { UsersService } from '../users.service';
 import { User } from '../user';
+// import router
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-view',
@@ -14,8 +16,9 @@ export class UserViewComponent implements OnInit {
   user: User;
 
   constructor(
-    private route: ActivatedRoute,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
@@ -29,5 +32,12 @@ export class UserViewComponent implements OnInit {
         this.user = response.user
       }
     );
+  }
+  deleteUser(id: string): void {
+    if(confirm("Are you sure to delete " + this.user.username)) {
+      this.usersService.deleteUser(id).subscribe(
+        ()=>{this.router.navigate(['/users'])}
+      );
+    }
   }
 }
