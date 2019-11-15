c30137aaab35423f5a32154270756b2c07b80691
ILIYA SOBOLEV <soboli@att.net>
2019-11-14 20:16:56 -0600

    Articles added

---
+++ src/app/article.ts
@@ -0,0 +1,7 @@
+export class Article {
+    _id: string;
+    slug: string;
+    title: string;
+    description: string;
+    username: string;
+  }
---
+++ src/app/articles/articles.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+// 1. Import the UserService
+import { ArticlesService } from '../article.service';
+
+// 2. Import the User Object/Schema
+import { Article } from '../article';
+
+@Component({
+  selector: 'articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss']
+})
+export class ArticlesComponent implements OnInit {
+
+  // 3. Create a users property of type user
+  articles: Article[];
+
+  // 4. Inject the UsersService into the constructor
+  constructor(private ArticlesService: ArticlesService) { }
+
+  // 6. Make a call to the service on initialization
+  ngOnInit() {
+    this.getArticles();
+  }
+
+  // 5. Craete a local wrapper for
+  getArticles(): void {
+    this.ArticlesService.getArticles().subscribe(
+      (response:any) => {
+        this.articles = response.articles
+//        console.log(this.users)
+      }
+    );
+  }
+}
